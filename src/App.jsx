c84e8c4588ea29105b27a93acc9dd4fbfb2d9831
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-04-02 00:20:49 +0530

    Completed project with a progress bar for 3 secs delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
@@ -51,7 +51,7 @@ function App() {
     };
   };
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -61,7 +61,8 @@ function App() {
     const newIds = storedIds.filter((id) => id !== selectedPlace.current);
     localStorage.setItem('selectedPlaces', JSON.stringify(newIds));
     localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
-  }
+  }, []);
+
 
   return (
     <>
--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,11 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ProgressBar from "./ProgressBar";
 
-export default function DeleteConfirmation({ onConfirm, onCancel }) {
+const TIMER = 3000;
+
+export default function DeleteConfirmation({ onCancel, onConfirm }) {
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
     return () => {
       clearTimeout(timer);
     }
@@ -23,6 +27,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
